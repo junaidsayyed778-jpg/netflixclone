635fc1d98ebb03fd64ee7814cb72fbc6812c42a7
Junaid Sayyed <junaidsayyed778@gmail.com>
2026-03-10 14:29:02 +0530

    fix scss path

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = require("./src/app");
 const connectDB = require("./src/config/database");
 
 connectDB()
-app.listen(3000, () =>{
-    console.log("server running on port 3000")
+
+app.listen(process.env.PORT || 3000, () =>{
+    console.log("server running on port " + (process.env.PORT || 3000))
 })
