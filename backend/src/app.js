1f5b58bf008cd140f15bfeedc5b25d930d61d3b2
Junaid Sayyed <junaidsayyed778@gmail.com>
2026-03-10 14:43:36 +0530

    add frontend url to cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser")
 const app = express();
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://netflixclone-frontend-tizx.onrender.com",
     credentials: true
 }))
 app.use(express.json());
